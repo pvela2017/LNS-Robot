2f7f101704513e273c4ef7a34be1c7fe1ac5f825
mefisto2017 <velaenator@gmail.com>
2023-03-30 15:12:39 +0900

    fixed all bugs

--- src/lns_bringup/src/driving_setup.cpp
+++ src/lns_bringup/src/driving_setup.cpp
@@ -1,3 +1,14 @@
+/*
+Setup driving motors in speed mode.
+
+Connect to socket 1
+
+TODO: Use ROS Param to load server ip and port
+
+by Pablo
+Last review: 2023/03/30
+*/
+
 #include <ros/ros.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
@@ -19,7 +30,7 @@ private:
 	int client_;
 	int status_;
 	
-	uint8_t bytes_out_[13] = {0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t bytes_out_[13] = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t bytes_in_[13];
     bool flag_success_ = false;
 
@@ -34,6 +45,10 @@ public:
 
 	int connectSocket()
 	{
+		/*
+		Create and connect socket
+		*/
+
 		// Create the socket 
 		if ((client_ = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     	{
@@ -70,14 +85,18 @@ public:
 
 	void sendCommand(int i)
 	{
+		/*
+		Send command and check the reply is ok (0x07)
+		*/
+
 		flag_success_ = false;
 		while (!flag_success_)
     	{
     		send(client_, bytes_out_, 13, MSG_WAITALL);
 
-	    	if (recv(client_, bytes_in_, 13, MSG_WAITALL) > 0) //attempts to read up to count bytes from file descriptor fd into the buffer starting at buf.
+	    	if (recv(client_, bytes_in_, 13, MSG_WAITALL) > 0) // reply its only 1 byte so total msg lenght 6 bytes (DLC, NC1, NC2, NC3, ID, D1).
 	    	{
-	    		// Check message received is correct
+	    		// Check message received is correct 0x07
 	        	if (bytes_in_[5] == 7)
 	        	{
 	        		flag_success_ = true; // PID: #06 setup correctly
@@ -88,12 +107,27 @@ public:
 	        	}
 	    	}
 	    	//ROS_DEBUG("%d", errno);	    	
-	    	ros::Duration(0.1).sleep(); // send command again every 10 ms
+	    	ros::Duration(1).sleep(); // send command again every 10 ms
 	    }    	
+	};
+
+	void sendCommandNoCheck()
+	{
+		/*
+		Send command without checking any reply from the motor controller
+		*/
+
+		send(client_, bytes_out_, 13, MSG_WAITALL);	    	
+	    ros::Duration(1).sleep(); // send command again every 10 ms  	
 	};
 
 	void onlyOnce(uint8_t motorId)
 	{
+		/*
+		Setup number of poles, motor direction, zero speed and brakes.
+		Only setup once in life!
+		*/
+
 		bytes_out_[4] = motorId;
 
 	    // Set number of poles  8 PID:#21 [Read Write (No need for 0xaa)]
@@ -103,7 +137,7 @@ public:
 	    setupMotor::sendCommand(1);
 
 	    // Set inversion of moving direction (Some motors) PID:#16 [Read Write (No need for 0xaa)]
-	    if (bytes_out_[4] == 0x02 || bytes_out_[4] == 0x03)
+	    if (bytes_out_[4] == 0x03 || bytes_out_[4] == 0x04)
 	    {
 	    	bytes_out_[5] = 0x10;
 	    	bytes_out_[6] = 0x01;
@@ -138,7 +172,18 @@ public:
 
 	void setMotor(uint8_t motorId)
 	{
+		/*
+		Setup motor controller in Speed Mode
+		*/
+
 		bytes_out_[4] = motorId;
+
+		// Dummy command, without sending this first the motor doesnt setup speed mode
+		// TODO: WHY???
+	    bytes_out_[5] = 0x86;
+	    bytes_out_[6] = 0x00;
+	    // Send command
+	    setupMotor::sendCommandNoCheck();
 
 	    // Set operation mode: speed   PID:#183 [Read Write (No need for 0xaa)] 
 	    bytes_out_[5] = 0xB7;
