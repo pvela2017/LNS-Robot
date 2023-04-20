2eb165078489be6cb94cf4fc16872ac7730155c8
mefisto2017 <velaenator@gmail.com>
2023-04-20 10:18:39 +0900

    Setup max steering angle

--- src/lns_teleop/scripts/teleop_keyboard.py
+++ src/lns_teleop/scripts/teleop_keyboard.py
@@ -196,7 +196,7 @@ if __name__=="__main__":
     rospy.init_node('teleop_twist_keyboard')
 
     speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+    turn = rospy.get_param("~turn", 0.9)
     speed_limit = rospy.get_param("~speed_limit", 1000)
     turn_limit = rospy.get_param("~turn_limit", 1000)
     repeat = rospy.get_param("~repeat_rate", 0.0)
