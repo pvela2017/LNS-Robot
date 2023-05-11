1dc9af0ad35fb8947151e9c7b2edc574a4810dea
mefisto2017 <velaenator@gmail.com>
2023-05-11 15:22:55 +0900

    Added media movil

--- src/driving/driving_pid/scripts/remap_feedback_node.py
+++ src/driving/driving_pid/scripts/remap_feedback_node.py
@@ -23,9 +23,28 @@ import numpy as np
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Float64
 
+
+motor_1_media_ = [0.0 , 0.0 , 0.0, 0.0 , 0.0]
+motor_2_media_ = [0.0 , 0.0 , 0.0, 0.0 , 0.0]
+motor_3_media_ = [0.0 , 0.0 , 0.0, 0.0 , 0.0]
+motor_4_media_ = [0.0 , 0.0 , 0.0, 0.0 , 0.0]
+
+
 def radTorpm(radsec):
-    wheel_rpm = (radsec *60.0)/(2.0*3.14)
-    return wheel_rpm;
+    motor_rpm = ((radsec *60.0)/(2.0*3.14))
+    return motor_rpm
+
+
+def media_movil(datos, ventana):
+    """
+    Aplica un filtro de media móvil de ventana 'ventana' a una serie de datos 'datos'.
+    """
+    ventana = int(ventana)
+    # Crea una matriz con una fila por cada punto de datos y una columna por cada punto de la ventana.
+    matriz = np.column_stack([datos[i: len(datos) - ventana + i + 1] for i in range(ventana)])
+    # Aplica la función de media en cada fila y devuelve la serie suavizada.
+    return np.mean(matriz, axis=1)
+
 
 def talker(msg):
     fb1_msg = Float64()
@@ -33,10 +52,24 @@ def talker(msg):
     fb3_msg = Float64()
     fb4_msg = Float64()
 
-    fb1_msg.data = radTorpm(msg.data[0])
-    fb2_msg.data = -radTorpm(msg.data[1])
-    fb3_msg.data = radTorpm(msg.data[2])
-    fb4_msg.data = -radTorpm(msg.data[3])
+    motor_1_media_.pop()
+    motor_1_media_.append(radTorpm(msg.data[0]))
+    fb1_msg.data = media_movil(motor_1_media_, 5)+0.9
+
+
+    motor_2_media_.pop()
+    motor_2_media_.append(-radTorpm(msg.data[1]))
+    fb2_msg.data = media_movil(motor_2_media_, 5)+0.9
+
+
+    motor_3_media_.pop()
+    motor_3_media_.append(radTorpm(msg.data[2]))
+    fb3_msg.data = media_movil(motor_3_media_, 5)+0.9
+
+
+    motor_4_media_.pop()
+    motor_4_media_.append(-radTorpm(msg.data[3]))
+    fb4_msg.data = media_movil(motor_4_media_, 5)+0.9
 
     fb_wheel_1_pub.publish(fb1_msg)
     fb_wheel_2_pub.publish(fb2_msg)
