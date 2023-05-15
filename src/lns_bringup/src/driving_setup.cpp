8f3708429c4117ddab3aeb4adc00842ba695d30f
mefisto2017 <velaenator@gmail.com>
2023-05-15 10:36:08 +0900

    Changed to CAN bus

--- src/lns_bringup/src/driving_setup.cpp
+++ src/lns_bringup/src/driving_setup.cpp
@@ -10,186 +10,107 @@ Last review: 2023/03/30
 */
 
 #include <ros/ros.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-
-
-#define SERVER_IP "192.168.0.7"
-#define PORT 20001
+#include <can_msgs/Frame.h>
 
 
 class setupMotor
 {
 private:
-	// Socket variables
-    struct sockaddr_in serv_addr_;
-	int client_;
-	int status_;
-	
-	uint8_t bytes_out_[13] = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t bytes_in_[13];
-    bool flag_success_ = false;
+	ros::NodeHandle n_;
+	ros::Subscriber receiveCAN_;
+	ros::Publisher sendtoCAN_;
+
+	// Internals
+	can_msgs::Frame message_;	
 
 public:
-	setupMotor()
-	{};
+	bool success_[4] = {false, false, false, false};
+	setupMotor(ros::NodeHandle n)
+	{
+		this->n_ = n;
+		this->receiveCAN_ = this->n_.subscribe<can_msgs::Frame>("/received_messages", 20, &setupMotor::canCB, this);
+    	this->sendtoCAN_ = this->n_.advertise<can_msgs::Frame>("/sent_messages", 10);
+	};
 	~setupMotor()
 	{
-		// Close the socket
-    	close(client_);
 	};
 
-	int connectSocket()
-	{
-		/*
-		Create and connect socket
-		*/
-
-		// Create the socket 
-		if ((client_ = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    	{
-	        ROS_FATAL("Socket creation error");
-	        return -1;
-	    }
-
-	    serv_addr_.sin_family = AF_INET;
-	    serv_addr_.sin_port = htons(PORT);
-	    
-	    if (inet_pton(AF_INET, SERVER_IP, &serv_addr_.sin_addr) <= 0)
-	    {
-	        ROS_FATAL("Invalid address");
-	        return -1;
-	    }
-
-	    // Set the client as non-blocking
-	    // On Linux, this command can change only the O_APPEND, O_ASYNC, O_DIRECT, O_NOATIME, and O_NONBLOCK flags.
-	    fcntl(client_, F_SETFL, O_NONBLOCK);
-
-	    // Connect to the server
-	    status_ = connect(client_, (struct sockaddr *)&serv_addr_, sizeof(serv_addr_));
-	    while (status_ < 0)
-	    {
-	        ROS_ERROR("Socket connection to the server failed");
-	        status_ = connect(client_, (struct sockaddr *)&serv_addr_, sizeof(serv_addr_));
-	        sleep(1);
-	    }
-
-	    // Connected
-	    ROS_INFO("Connected to the server successfully");
-	    return 0;
-	};
-
-	void sendCommand(int i)
-	{
-		/*
-		Send command and check the reply is ok (0x07)
-		*/
-
-		flag_success_ = false;
-		while (!flag_success_)
-    	{
-    		send(client_, bytes_out_, 13, MSG_WAITALL);
-
-	    	if (recv(client_, bytes_in_, 13, MSG_WAITALL) > 0) // reply its only 1 byte so total msg lenght 6 bytes (DLC, NC1, NC2, NC3, ID, D1).
-	    	{
-	    		// Check message received is correct 0x07
-	        	if (bytes_in_[5] == 7)
-	        	{
-	        		flag_success_ = true; // PID: #06 setup correctly
-	        	}
-	        	else
-	        	{
-	        		ROS_ERROR("Setup %d Failed", i);
-	        	}
-	    	}
-	    	//ROS_DEBUG("%d", errno);	    	
-	    	ros::Duration(1).sleep(); // send command again every 10 ms
-	    }    	
-	};
-
-	void sendCommandNoCheck()
-	{
-		/*
-		Send command without checking any reply from the motor controller
-		*/
-
-		send(client_, bytes_out_, 13, MSG_WAITALL);	    	
-	    ros::Duration(1).sleep(); // send command again every 10 ms  	
-	};
-
-	void onlyOnce(uint8_t motorId)
-	{
-		/*
-		Setup number of poles, motor direction, zero speed and brakes.
-		Only setup once in life!
-		*/
-
-		bytes_out_[4] = motorId;
-
-	    // Set number of poles  8 PID:#21 [Read Write (No need for 0xaa)]
-	    bytes_out_[5] = 0x15;
-	    bytes_out_[6] = 0x01;
-	    // Send command
-	    setupMotor::sendCommand(1);
-
-	    // Set inversion of moving direction (Some motors) PID:#16 [Read Write (No need for 0xaa)]
-	    if (bytes_out_[4] == 0x03 || bytes_out_[4] == 0x04)
-	    {
-	    	bytes_out_[5] = 0x10;
-	    	bytes_out_[6] = 0x01;
-	    }
-	    else
-	    {
-	    	bytes_out_[5] = 0x10;
-	    	bytes_out_[6] = 0x00;
-	    }
-	    // Send command
-	    setupMotor::sendCommand(2);
-
-	    // Set speed = 0 then control zero speed, not braking  PID:#24 [Read Write (No need for 0xaa)]
-	    bytes_out_[5] = 0x18;
-	    bytes_out_[6] = 0x01;
-	    // Send command
-	    setupMotor::sendCommand(3);
-
-	    // Set operation mode: speed   PID:#183 [Read Write (No need for 0xaa)] 
-	    bytes_out_[5] = 0xB7;
-	    bytes_out_[6] = 0x01;
-	    // Send command
-	    setupMotor::sendCommand(4);
-
-	   	// Free mode on, brake OFF  PID:#05 [Command]
-	    bytes_out_[5] = 0x05;
-	    bytes_out_[6] = 0x00;
-	    // Send command
-	    setupMotor::sendCommand(5);
-	};
-
-
-	void setMotor(uint8_t motorId)
+	void setMotor(uint32_t idmotor)
 	{
 		/*
 		Setup motor controller in Speed Mode
 		*/
 
-		bytes_out_[4] = motorId;
+		// Set message
+	    message_.is_rtr = false;
+	    message_.is_extended = false;
+	    message_.is_error = false;
+	    message_.dlc = 8;
 
-		// Dummy command, without sending this first the motor doesnt setup speed mode
-		// TODO: WHY???
-	    bytes_out_[5] = 0x86;
-	    bytes_out_[6] = 0x00;
-	    // Send command
-	    setupMotor::sendCommandNoCheck();
 
+	    // Broadcast
+	    message_.id = idmotor;
+
+
+	    // Dummy
+	    message_.data[0] = 0x86;
+	    for (int i = 1; i < 8; i++)
+    	{
+        	message_.data[i] = 0;
+    	}
+    	ros::Duration(1).sleep();
+    	sendtoCAN_.publish(message_);
+    	ros::Duration(1).sleep();
+	    
 	    // Set operation mode: speed   PID:#183 [Read Write (No need for 0xaa)] 
-	    bytes_out_[5] = 0xB7;
-	    bytes_out_[6] = 0x01;
-	    // Send command
-	    setupMotor::sendCommand(1);
+	    message_.data[0] = 0xB7;
+	    message_.data[1] = 0x01;
+	    for (int i = 2; i < 8; i++)
+    	{
+        	message_.data[i] = 0;
+    	}
+    	sendtoCAN_.publish(message_);
+    	ros::spinOnce();
+	};
+
+
+	void canCB(const can_msgs::Frame::ConstPtr& msg)
+	{
+    	/**/
+	    switch(msg->id) 
+	    {
+	        case 1793: // motor 1 
+	            if (msg->data[0] == 7 && msg->data[1] == 183) // confirm B7 command
+	            {
+	                success_[0] = true;
+	            }
+	            break;
+
+	        case 1794: // motor 2
+	            if (msg->data[0] == 7 && msg->data[1] == 183) // confirm B7 command
+	            {
+	                success_[1] = true;
+	            }
+	            break;
+
+	        case 1795: // motor 3
+	            if (msg->data[0] == 7 && msg->data[1] == 183) // confirm B7 command
+	            {
+	                success_[2] = true;
+	            }
+	            break;
+
+	        case 1796: // motor 4
+	            if (msg->data[0] == 7 && msg->data[1] == 183) // confirm B7 command
+	            {
+	                success_[3] = true;
+	            }
+	            break;
+
+	        default:
+	            break;
+	    }
+
 	};
 
 };
@@ -198,23 +119,21 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "driving_setup");
     ros::NodeHandle n;
+    ros::AsyncSpinner spinner(2);
+    spinner.start();
     
-    setupMotor drivingMotors;
-    drivingMotors.connectSocket();
+    setupMotor drivingMotors(n);
 
-    // Motor 1
-    drivingMotors.setMotor(0x01);
-    ROS_INFO("Motor 1 OK");
 
-    // Motor 2
-    drivingMotors.setMotor(0x02);
-    ROS_INFO("Motor 2 OK");
+    while (!drivingMotors.success_[0] && !drivingMotors.success_[1] && !drivingMotors.success_[2] && !drivingMotors.success_[3])
+    {
+    	drivingMotors.setMotor(0x01);
+    	drivingMotors.setMotor(0x02); 
+    	drivingMotors.setMotor(0x03); 
+    	drivingMotors.setMotor(0x04);         
+    }
 
-    // Motor 3
-    drivingMotors.setMotor(0x03);
-    ROS_INFO("Motor 3 OK");
-
-    // Motor 4
-    drivingMotors.setMotor(0x04);
-    ROS_INFO("Motor 4 OK");
+    
+    ROS_INFO("All motors OK");
+    spinner.stop();
 }
