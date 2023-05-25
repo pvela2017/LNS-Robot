0f447af3eef80d7d9a48430e50f1649ea947f69a
mefisto2017 <velaenator@gmail.com>
2023-05-25 20:01:04 +0900

    Fix oscillation in steady state

--- src/driving/driving_pid/src/pid.cpp
+++ src/driving/driving_pid/src/pid.cpp
@@ -192,7 +192,7 @@ void PidObject::doCalcs()
     Added deadband
     TODO: Add as parameter
     */
-    float deadband = 0.02; // 0.2rpm error
+    float deadband = 0.03; // 0.9rpm error
     if (fabsf(error_.at(0)) < deadband)
     {
       error_.at(0) = 0.0;
